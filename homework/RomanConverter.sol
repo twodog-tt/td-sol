883bc299f240d5b569e98b77d180bcd6cfb2585c
二狗同学 <twodogtt01@gmail.com>
2025-05-24 23:04:57 +0800

    add

--- homework/RomanConverter.sol
+++ homework/RomanConverter.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract RomanConverter {
-    mapping(bytes1 => uint256) public symbolValues;
+    mapping(bytes1 => uint256) public symbolValues; // 映射：存储罗马字符的对应数值
 
     constructor() {
         symbolValues["I"] = 1;
@@ -15,15 +15,16 @@ contract RomanConverter {
     }
 
     function romanToInt(string memory s) public view returns (uint256) {
-        bytes memory str = bytes(s);
-        uint256 n = str.length;
-        uint256 ans = 0;
+        bytes memory str = bytes(s); // 转换成bytes
+        uint256 n = str.length; // 获取长度
+        uint256 ans = 0; // 初始化返回值
 
-        for (uint256 i = 0; i < n; i++) {
+        for (uint256 i = 0; i < n; i++) { // 遍历字符数组bytes
             uint256 value = symbolValues[str[i]];
+            // 如果当前字符比后一个字符小，说明是减法结构（如 IV = 5 - 1 = 4），减去当前值。
             if (i < n - 1 && value < symbolValues[str[i + 1]]) {
                 ans -= value;
-            } else {
+            } else {// 否则是加法结构，加上当前值。
                 ans += value;
             }
         }
